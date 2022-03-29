c478c27b0fa6f43844ed550661a56badd2bfc69f
Rupal Patel <rupal@drivelah.sg>
2022-03-29 11:59:53 +0530

    feat: Add scaling into metrics

---
+++ src/themes/Metrics.ts
@@ -0,0 +1,16 @@
+import { Dimensions, PixelRatio } from 'react-native';
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+const guidelineBaseWidth = 350; // change this based upon the designs given
+const guidelineBaseHeight = 680; // change this based upon the designs given
+
+export const scaleSize = (size: number) =>
+  (WINDOW_WIDTH / guidelineBaseWidth) * size;
+
+export const moderateScale = (size: number, factor: number = 0.5) =>
+  size + (scaleSize(size) - size) * factor;
+
+export const verticalScale = (size: number) =>
+  (WINDOW_HEIGHT / guidelineBaseHeight) * size;
+
+export const scaleFont = (size: number) => size * PixelRatio.getFontScale();
